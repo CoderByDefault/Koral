f512b15263dd040a024d311b15811cacbd4a5cf2
Arsalan Gul <gul@mail.uni-paderborn.de>
2021-01-17 11:17:18 +0100

    bug fixes
    Added missing methods in ConfigurationSerializer.java which caused NoSuchMethodFound Exception

--- src/main/java/de/uni_koblenz/west/koral/common/config/impl/ConfigurationSerializer.java
+++ src/main/java/de/uni_koblenz/west/koral/common/config/impl/ConfigurationSerializer.java
@@ -28,113 +28,144 @@ import de.uni_koblenz.west.koral.common.config.ConfigurableSerializer;
 
 public class ConfigurationSerializer implements ConfigurableSerializer {
 
-  public String serializeMaster(Configuration conf) {
-    String[] master = conf.getMaster();
-    if (master[0] != null) {
-      return master[0] + ":" + master[1];
-    } else {
-      return "";
-    }
-  }
+	public String serializeMaster(Configuration conf) {
+		final String[] master = conf.getMaster();
+		if (master[0] != null) {
+			return master[0] + ":" + master[1];
+		} else {
+			return "";
+		}
+	}
 
-  public String serializeFtpServer(Configuration conf) {
-    String[] ftpServer = conf.getFTPServer();
-    if (ftpServer[0] != null) {
-      return ftpServer[0] + ":" + ftpServer[1];
-    } else {
-      return "";
-    }
-  }
+	public String serializeFtpServer(Configuration conf) {
+		final String[] ftpServer = conf.getFTPServer();
+		if (ftpServer[0] != null) {
+			return ftpServer[0] + ":" + ftpServer[1];
+		} else {
+			return "";
+		}
+	}
 
-  public String serializeSlaves(Configuration conf) {
-    StringBuilder sb = new StringBuilder();
-    String delim = "";
-    for (int i = 0; i < conf.getNumberOfSlaves(); i++) {
-      String[] slave = conf.getSlave(i);
-      sb.append(delim).append(slave[0]).append(":").append(slave[1]);
-      delim = ",";
-    }
-    return sb.toString();
-  }
+	public String serializeSlaves(Configuration conf) {
+		final StringBuilder sb = new StringBuilder();
+		String delim = "";
+		for (int i = 0; i < conf.getNumberOfSlaves(); i++) {
+			final String[] slave = conf.getSlave(i);
+			sb.append(delim).append(slave[0]).append(":").append(slave[1]);
+			delim = ",";
+		}
+		return sb.toString();
+	}
 
-  public String serializeClientConnection(Configuration conf) {
-    String[] client = conf.getClient();
-    if (client[0] != null) {
-      return client[0] + ":" + client[1];
-    } else {
-      return "";
-    }
-  }
+	public String serializeClientConnection(Configuration conf) {
+		final String[] client = conf.getClient();
+		if (client[0] != null) {
+			return client[0] + ":" + client[1];
+		} else {
+			return "";
+		}
+	}
 
-  public String serializeClientConnectionTimeout(Configuration conf) {
-    return Long.valueOf(conf.getClientConnectionTimeout()).toString();
-  }
+	public String serializeClientConnectionTimeout(Configuration conf) {
+		return Long.valueOf(conf.getClientConnectionTimeout()).toString();
+	}
 
-  public String serializeLogLevel(Configuration conf) {
-    return conf.getLoglevel().getName();
-  }
+	public String serializeLogLevel(Configuration conf) {
+		return conf.getLoglevel().getName();
+	}
 
-  public String serializeTmpDir(Configuration conf) {
-    return conf.getTmpDir();
-  }
+	public String serializeTmpDir(Configuration conf) {
+		return conf.getTmpDir();
+	}
 
-  public String serializeDataDir(Configuration conf) {
-    return conf.getDataDir();
-  }
+	public String serializeDataDir(Configuration conf) {
+		return conf.getDataDir();
+	}
 
-  public String serializeMaxDictionaryWriteBatchSize(Configuration conf) {
-    return Integer.valueOf(conf.getMaxDictionaryWriteBatchSize()).toString();
-  }
+	public String serializeMaxDictionaryWriteBatchSize(Configuration conf) {
+		return Integer.valueOf(conf.getMaxDictionaryWriteBatchSize()).toString();
+	}
 
-  public String serializeEnableTransactionsForTripleStore(Configuration conf) {
-    return Boolean.valueOf(conf.useTransactionsForTripleStore()).toString();
-  }
+	public String serializeEnableTransactionsForTripleStore(Configuration conf) {
+		return Boolean.valueOf(conf.useTransactionsForTripleStore()).toString();
+	}
 
-  public String serializeSizeOfMappingRecycleCache(Configuration conf) {
-    return Integer.valueOf(conf.getSizeOfMappingRecycleCache()).toString();
-  }
+	public String serializeSizeOfMappingRecycleCache(Configuration conf) {
+		return Integer.valueOf(conf.getSizeOfMappingRecycleCache()).toString();
+	}
 
-  public String serializeUnbalanceThresholdForWorkerThreads(Configuration conf) {
-    return Double.valueOf(conf.getUnbalanceThresholdForWorkerThreads()).toString();
-  }
+	public String serializeUnbalanceThresholdForWorkerThreads(Configuration conf) {
+		return Double.valueOf(conf.getUnbalanceThresholdForWorkerThreads()).toString();
+	}
 
-  public String serializeMappingBundleSize(Configuration conf) {
-    return Integer.valueOf(conf.getMappingBundleSize()).toString();
-  }
+	public String serializeMappingBundleSize(Configuration conf) {
+		return Integer.valueOf(conf.getMappingBundleSize()).toString();
+	}
 
-  public String serializeReceiverQueueSize(Configuration conf) {
-    return Integer.valueOf(conf.getReceiverQueueSize()).toString();
-  }
+	public String serializeReceiverQueueSize(Configuration conf) {
+		return Integer.valueOf(conf.getReceiverQueueSize()).toString();
+	}
 
-  public String serializeMappingsPerOperationRound(Configuration conf) {
-    return Integer.valueOf(conf.getMaxEmittedMappingsPerRound()).toString();
-  }
+	public String serializeMappingsPerOperationRound(Configuration conf) {
+		return Integer.valueOf(conf.getMaxEmittedMappingsPerRound()).toString();
+	}
 
-  public String serializeEnableTransactionsForJoinCache(Configuration conf) {
-    return Boolean.valueOf(conf.useTransactionsForJoinCache()).toString();
-  }
+	public String serializeEnableTransactionsForJoinCache(Configuration conf) {
+		return Boolean.valueOf(conf.useTransactionsForJoinCache()).toString();
+	}
 
-  public String serializeRowDataLength(Configuration conf) {
-    return Integer.valueOf(conf.getRowDataLength()).toString();
-  }
+	public String serializeRowDataLength(Configuration conf) {
+		return Integer.valueOf(conf.getRowDataLength()).toString();
+	}
 
-  public String serializeIndexCacheSize(Configuration conf) {
-    return Integer.valueOf(conf.getIndexCacheSize()).toString();
-  }
+	public String serializeIndexCacheSize(Configuration conf) {
+		return Integer.valueOf(conf.getIndexCacheSize()).toString();
+	}
 
-  public String serializeExtraCacheSize(Configuration conf) {
-    return Integer.valueOf(conf.getExtraCacheSize()).toString();
-  }
+	public String serializeExtraCacheSize(Configuration conf) {
+		return Integer.valueOf(conf.getExtraCacheSize()).toString();
+	}
 
-  public String serializeRecyclerCapacity(Configuration conf) {
-    return Integer.valueOf(conf.getRecyclerCapacity()).toString();
-  }
+	public String serializeRecyclerCapacity(Configuration conf) {
+		return Integer.valueOf(conf.getRecyclerCapacity()).toString();
+	}
 
-  public String serializeBlockSize(Configuration conf) {
-    return Integer.valueOf(conf.getBlockSize()).toString();
-  }
+	public String serializeBlockSize(Configuration conf) {
+		return Integer.valueOf(conf.getBlockSize()).toString();
+	}
 
-  public String serializeMaxOpenFiles(Configuration conf) {
-    return Integer.valueOf(conf.getMaxOpenFiles()).toString();
-  }
+	public String serializeMaxOpenFiles(Configuration conf) {
+		return Integer.valueOf(conf.getMaxOpenFiles()).toString();
+	}
+
+//Arsalan Gul's - Start //
+	public String serializeTripleStoreStorageType(Configuration conf) {
+		return conf.getTripleStoreStorageType().toString();
+	}
+
+	public String serializeTripleStoreCacheType(Configuration conf) {
+		return conf.getTripleStoreCacheType().toString();
+	}
+
+	public String serializeJoinCacheStorageType(Configuration conf) {
+		return conf.getJoinCacheStorageType().toString();
+	}
+
+	public String serializeEnableAsynchronousWritesForTripleStore(Configuration conf) {
+		return Boolean.valueOf(conf.isTripleStoreAsynchronouslyWritten()).toString();
+	}
+
+	public String serializeEnableAsynchronousWritesForJoinCache(Configuration conf) {
+		return Boolean.valueOf(conf.isJoinCacheAsynchronouslyWritten()).toString();
+	}
+
+	public String serializeJoinCacheType(Configuration conf) {
+		return conf.getJoinCacheType().toString();
+	}
+
+	public String serializeExtraFilesCacheSize(Configuration conf) {
+		return Integer.valueOf(conf.getExtraCacheSize()).toString();
+	}
+
+	// Arsalan Gul's - End //
 }
